16f7b0f2b63437e0efcc7455c679a11f85ce14d0
Almaz <almazius11@gmail.com>
2023-02-11 18:47:54 +0300

    created basic structure of project

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+function Navigation() {
+  return (
+    <div className="flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white">
+      <h3 className="font-bold">Github Поиск</h3>
+      <span>
+        <Link to="/githubSearch" className="mr-2">
+          Главная
+        </Link>
+        <Link to="/githubSearch/favourites">Избранное</Link>
+      </span>
+    </div>
+  )
+}
+
+export default Navigation
